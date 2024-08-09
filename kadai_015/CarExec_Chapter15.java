371a60ddab7692e7bf96052c24a9aca1aa7e5048
tuyokairu <yuri492@yahoo.co.jp>
2024-08-10 03:37:02 +0900

    車クラスを作って速度を出力しよう

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,22 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		//変数名は具体的に
+		Car_Chapter15 toyota = new Car_Chapter15( 1, 10);//インスタンス化する
+		
+		
+		toyota.gearChange(3);
+		toyota.run();
+		
+		
+		}
+	
+
+	}
+
+
