c4cf225a5f9f3d45d048d72c1dd64ee2933f88b0
tuyokairu <yuri492@yahoo.co.jp>
2024-09-01 16:44:14 +0900

    継承を使って自分と家族を紹介しよう

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -3,18 +3,18 @@ package kadai_018;
 abstract public class Kato_Chapter18 {
 	
 	//姓フィールド
-	public String familyName;
+	public String familyName ="加藤";
+	
 	//名フィールド
 	public String givenName;
+	
 	//住所フィールド
-	public String address;
+	public String address = "東京都中野区○×";
 	
 
 	//抽象メソッド(中に何も入れない)
 	public void commonIntroduce() {
 		//famirynameとaddressを外に出して、下のSystem.outの部分をここに持ってくる。
-		familyName = "加藤";
-		address = "東京都中野区○×";
 		
 		System.out.println("名前は" + familyName + givenName + "です");
 		System.out.println("住所は" + address + "です");
